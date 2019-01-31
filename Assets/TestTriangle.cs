c82bfd31e2831ff02e7f1fd60b2098e282fae7a8
warrickct <wcor690@aucklanduni.ac.nz>
2019-01-31 14:45:40 +1300

    generating triangles for tube cap correctly.

--- Assets/TestTriangle.cs
+++ Assets/TestTriangle.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using System;
 
 public class TestTriangle : MonoBehaviour {
 
@@ -17,6 +18,32 @@ public class TestTriangle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		SingleCircleTest();
+	}
+
+	private void SingleCircleTest()
+	{
+		Vector3[] verts = GameObject.FindGameObjectsWithTag("vert").OrderBy(b => b.transform.parent.name).Select(v => v.transform.position).ToArray();
+		List<int> tris = new List<int>();
+		for (int i = 1; i < verts.Length - 1; i++)
+		{
+			tris.Add(0);
+			tris.Add(i);
+			tris.Add(i + 1);
+		}
+		var mesh = new Mesh();
+		mesh.vertices = verts;
+		mesh.triangles = tris.ToArray();
+		mesh.RecalculateNormals();
+		var mFilter = GetComponent<MeshFilter>();
+		mFilter.sharedMesh = mesh;
+		// material
+		var mat = GameObject.FindGameObjectWithTag("vert").GetComponent<MeshRenderer>().material;
+		var mRend = GetComponent<MeshRenderer>();
+		mRend.material = mat;
+	}
+
+	void SingleTriangleTest(){
 		// mesh
 		var points = GameObject.FindGameObjectsWithTag("vert");
 		var mesh = new Mesh();
